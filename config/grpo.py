c3dbddc9936f3adaf1d3ca21f3d289efc46949f3
amannier <chenyihuang0704@gmail.com>
2025-11-03 15:58:12 +0800

    11.3 new geneval config

--- config/grpo.py
+++ config/grpo.py
@@ -161,6 +161,94 @@ def geneval_sd3():
     config.per_prompt_stat_tracking = True
     return config
 
+
+
+def geneval_sd3_new_ori():
+    gpu_number = 24
+    config = compressibility()
+    config.dataset = os.path.join(os.getcwd(), "dataset/geneval")
+
+    # sd3.5 medium
+    config.pretrained.model = "stabilityai/stable-diffusion-3.5-medium"
+    config.sample.num_steps = 10
+    config.sample.eval_num_steps = 40
+    config.sample.guidance_scale = 4.5
+
+    config.resolution = 512
+    config.sample.train_batch_size = 8
+    config.sample.num_image_per_prompt = 24
+    config.sample.num_batches_per_epoch = int(48/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt))
+    assert config.sample.num_batches_per_epoch % 2 == 0, "Please set config.sample.num_batches_per_epoch to an even number! This ensures that config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch / 2, so that gradients are updated twice per epoch."
+    config.sample.test_batch_size = 14 # This bs is a special design, the test set has a total of 2212, to make gpu_num*bs*n as close as possible to 2212, because when the number of samples cannot be divided evenly by the number of cards, multi-card will fill the last batch to ensure each card has the same number of samples, affecting gradient synchronization.
+
+    config.train.batch_size = config.sample.train_batch_size
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch//2
+    config.train.num_inner_epochs = 1
+    config.train.timestep_fraction = 0.99
+    config.train.beta = 0.04
+    config.sample.global_std = True
+    config.sample.same_latent = False
+    config.train.ema = True
+    config.save_freq = 60 # epoch
+    config.eval_freq = 60
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    config.run_name = f'geneval_ori-24_bs_8'
+    config.save_dir = f'logs/geneval/{config.run_name}_{timestamp}'
+    config.reward_fn = {
+        "geneval": 1.0,
+    }
+    
+    config.prompt_fn = "geneval"
+
+    config.per_prompt_stat_tracking = True
+    return config
+
+
+
+
+def geneval_sd3_ori_2gpu():
+    gpu_number = 2
+    config = compressibility()
+    config.dataset = os.path.join(os.getcwd(), "dataset/geneval")
+
+    # sd3.5 medium
+    config.pretrained.model = "/data11/xinyue.liu/sjy/TempFlow-GRPO/base_models/models--stabilityai--stable-diffusion-3.5-medium/b940f670f0eda2d07fbb75229e779da1ad11eb80"
+    config.sample.num_steps = 10
+    config.sample.eval_num_steps = 40
+    config.sample.guidance_scale = 4.5
+
+    config.resolution = 512
+    config.sample.train_batch_size = 8
+    config.sample.num_image_per_prompt = 16
+    config.sample.num_batches_per_epoch = int(8/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt))
+    assert config.sample.num_batches_per_epoch % 2 == 0, "Please set config.sample.num_batches_per_epoch to an even number! This ensures that config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch / 2, so that gradients are updated twice per epoch."
+    config.sample.test_batch_size = 14 # This bs is a special design, the test set has a total of 2212, to make gpu_num*bs*n as close as possible to 2212, because when the number of samples cannot be divided evenly by the number of cards, multi-card will fill the last batch to ensure each card has the same number of samples, affecting gradient synchronization.
+
+    config.train.batch_size = config.sample.train_batch_size
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch//2
+    config.train.num_inner_epochs = 1
+    config.train.timestep_fraction = 0.99
+    config.train.beta = 0.04
+    config.sample.global_std = True
+    config.sample.same_latent = False
+    config.train.ema = True
+    config.save_freq = 60 # epoch
+    config.eval_freq = 60
+    # 新增：生成时间戳
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # 更新：将时间戳附加到 save_dir
+    config.run_name = f'geneval_ori-16'
+    config.save_dir = f'logs/geneval/{config.run_name}_{timestamp}'
+    # config.save_dir = f'logs/geneval/sd3.5-M'
+    config.reward_fn = {
+        "geneval": 1.0,
+    }
+    
+    config.prompt_fn = "geneval"
+
+    config.per_prompt_stat_tracking = True
+    return config
+
 def pickscore_sd3():
     gpu_number=32
     config = compressibility()
@@ -700,6 +788,57 @@ def pickscore_sd3_pruning_2gpu():
     }
     
     config.prompt_fn = "general_ocr"
+
+    config.per_prompt_stat_tracking = True
+    return config
+
+def geneval_sd3_pruning_2gpu():
+    gpu_number = 2
+    config = compressibility()
+    config.dataset = os.path.join(os.getcwd(), "dataset/geneval")
+
+    # sd3.5 medium
+    config.pretrained.model = "/data11/xinyue.liu/sjy/TempFlow-GRPO/base_models/models--stabilityai--stable-diffusion-3.5-medium/b940f670f0eda2d07fbb75229e779da1ad11eb80"
+    config.pretrained.reward_model = 'geneval'
+    config.sample.num_steps = 10
+    config.sample.eval_num_steps = 40
+    config.sample.guidance_scale = 4.5
+
+    config.resolution = 512
+    config.sample.train_batch_size = 8
+    config.sample.num_image_per_prompt_before_pruning = 8
+    config.sample.skip_timesteps = [5,7]
+    config.sample.lefts = [7,4]
+    config.sample.num_image_per_prompt = config.sample.lefts[-1]
+    # 所有进程一次batch总共算了多少不同的prompt
+    config.sample.unique_prompts = int(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt_before_pruning)
+    # 加一个sample_batches_per_epoch，用于记录每次采样多少个batch，因为每次采样多少个batch是根据num_image_per_prompt_before_pruning计算的，所以需要一个变量来记录
+    config.sample.sample_batches_per_epoch = int(8/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt_before_pruning))
+    config.sample.num_batches_per_epoch = int(8/(gpu_number*config.sample.train_batch_size/config.sample.num_image_per_prompt))
+    assert config.sample.num_batches_per_epoch % 2 == 0, "Please set config.sample.num_batches_per_epoch to an even number! This ensures that config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch / 2, so that gradients are updated twice per epoch."
+    config.sample.test_batch_size = 14 # This bs is a special design, the test set has a total of 2212, to make gpu_num*bs*n as close as possible to 2212, because when the number of samples cannot be divided evenly by the number of cards, multi-card will fill the last batch to ensure each card has the same number of samples, affecting gradient synchronization.
+
+    config.train.batch_size = config.sample.train_batch_size
+    config.train.gradient_accumulation_steps = config.sample.num_batches_per_epoch//2
+    config.train.num_inner_epochs = 1
+    config.train.timestep_fraction = 0.99
+    config.train.beta = 0.04
+    config.sample.global_std = True
+    config.sample.same_latent = False
+    config.train.ema = True
+    config.save_freq = 60 # epoch
+    config.eval_freq = 60
+    # 新增：生成时间戳
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+    # 更新：将时间戳附加到 save_dir
+    config.run_name = f'geneval_pruning-8-to-4'
+    config.save_dir = f'logs/geneval/{config.run_name}_{timestamp}'
+    # config.save_dir = f'logs/geneval/sd3.5-M'
+    config.reward_fn = {
+        "geneval": 1.0,
+    }
+    
+    config.prompt_fn = "geneval"
 
     config.per_prompt_stat_tracking = True
     return config
